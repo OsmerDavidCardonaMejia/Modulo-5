4b9aa40824793f27789da48148baee1418b8a64e
OsmerDavidCardonaMejia <juliomidencearias2000@gmail.com>
2024-11-06 22:21:26 -0600

    Formas
    Esta clase es la llamada Super Clase

---
+++ Formas.java
@@ -0,0 +1,33 @@
+//Osmer David Cardona Mejia
+//NC:202410050140
+
+public class Formas
+{
+    private String color;
+    public void establecerColor(String color)
+    {
+        this.color = color;
+    }
+    
+    public void dibujar()
+    {
+        System.out.println("Dibujando una forma generica");
+    }
+    
+    public double getRadio() 
+    {
+        return 0;
+    }
+     public double getLargo() 
+    {
+        return 0;
+    }
+      public double getAngulo() 
+    {
+        return 0;
+    }
+       public double getArea() 
+    {
+        return 0;
+    }
+}
